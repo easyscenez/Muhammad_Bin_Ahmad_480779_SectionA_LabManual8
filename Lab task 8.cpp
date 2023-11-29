588d48e30128b702ffda13cfaaa07dd0ceb6bb3b
easyscenez <147534619+easyscenez@users.noreply.github.com>
2023-11-29 10:26:34 +0500

    Add files via upload

---
+++ Lab task 8.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+//	int num_array[10];
+//	float sum=0;
+//	cout<<"Enter 10 numbers in the array"<<endl;
+//	for (int i=0;i<10;i++){
+//		cin>>num_array[i];
+//	}
+//	for(int j=0;j<10;j++){
+//		sum+=num_array[j];
+//	}
+//	cout<<"The average of the numbers in the array is "<<sum/10;
+	// end of task 1
+	
+//	int temp;
+//	int num_array[5];
+//	cout<<"Enter 5 numbers into the array, one at a time."<<endl;
+//	for (int i=0;i<5;i++){
+//		cin>>num_array[i];
+//	}
+//	for (int j=0;j<5;j++){
+//		for(int k=0;k<4;k++){
+//			if(num_array[k]>num_array[k+1]){
+//				temp=num_array[k];
+//				num_array[k]=num_array[k+1];
+//				num_array[k+1]=temp;
+//			}
+//		}
+//	}
+//	cout<<endl;
+//	cout<<"The rearranged array is "<<endl;
+//	for (int n=0;n<5;n++){
+//		cout<<num_array[n]<<" ";
+//	}
+	// end of task 2
+	
+//	int numbers[5];
+//	int min;
+//	cout<<"Enter 5 numbers into the array one at a time."<<endl;
+//	for(int i=0;i<5;i++){
+//		cin>>numbers[i];
+//	}
+//	for (int i=0;i<5;i++){
+//		min=numbers[i];
+//		for (int j=i;j<5;j++){
+//			if(min>numbers[j]){
+//				min=numbers[j];
+//				numbers[j]=numbers[i];
+//				numbers[i]=min;
+//			}
+//		}
+//	}
+//	for(int i=0;i<5;i++){
+//		cout<<numbers[i]<<" ";
+//	}
+	// end of task 3
+	
+}
